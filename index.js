afbca18e15d6fb13fe0e5be6dcad0518ce7ce915
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-10-12 17:48:25 +0200

    algo1

---
+++ index.js
@@ -0,0 +1,11 @@
+let char = String.fromCharCode(97);
+
+
+//let string = 'a'
+//console.log(string.charCodeAt(0))
+
+
+
+for(let index=97;index<123;index++){
+    console.log(String.fromCharCode(index))
+}

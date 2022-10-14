802561137a3ff786ed778720a4653a82511f1f5f
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-10-14 13:57:25 +0200

    algo1

--- alphabet.js
+++ alphabet.js
@@ -1,11 +1,15 @@
+//exercice
 let char = String.fromCharCode(97);
-
 
 //let string = 'a'
 //console.log(string.charCodeAt(0))
 
-
-
 for(let index=97;index<123;index++){
     console.log(String.fromCharCode(index))
+}
+
+//corection
+let char = 97
+for (let i = 0; i < 26; i++) {
+    console.log(String.fromCharCode(char + i ));
 }

ed636233663b6ac1dd71cc07ae1f45ab655c7533
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-10-14 13:52:26 +0200

    algo3

---
+++ arg.js
@@ -0,0 +1,12 @@
+//exercice
+for(let i = 2; i < process.argv.length; i++) {
+   console.log(process.argv[i]);
+}
+
+//correction
+let args = process.argv.slice(2);
+
+for(let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+}
+

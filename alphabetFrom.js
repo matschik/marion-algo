d34b86dd23159606813a47ff3a76b8e5cf5f1273
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-10-18 12:08:47 +0200

    algo4

---
+++ alphabetFrom.js
@@ -0,0 +1,16 @@
+//exercice
+
+let args = process.argv.slice(2);
+let inputChar = args[0];
+let inputCharCode = inputChar.charCodeAt(0);
+let nbLoop = "z".charCodeAt(0) - inputCharCode;
+for (let i = 0; i <= nbLoop; i++) {
+  console.log(String.fromCharCode(inputCharCode + i));
+}
+
+//console.log(
+//String.fromCharCode(inputCharCode + 1),
+//String.fromCharCode(inputCharCode + 2),
+//String.fromCharCode(inputCharCode + 3),
+//String.fromCharCode(inputCharCode + 4)
+//)

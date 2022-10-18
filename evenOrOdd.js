c87a46d0a90dd7260778a830332187bb501456b7
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-10-18 16:40:11 +0200

    algo5

---
+++ evenOrOdd.js
@@ -0,0 +1,14 @@
+let args = process.argv.slice(2);
+let inputNb = args[0];
+let num = Number(inputNb);
+
+if (!isNaN(num)) {
+  let isEven = inputNb % 2 === 0;
+  if (isEven) {
+    console.log("pair");
+  } else {
+    console.log("impair");
+  }
+} else {
+  console.log("impossible");
+}

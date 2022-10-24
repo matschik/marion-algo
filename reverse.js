5b5f8ac333394731e6f829cb613a81ce4a7b92d7
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-10-24 20:28:54 +0200

    algo7

---
+++ reverse.js
@@ -0,0 +1,17 @@
+let args = process.argv.slice(2);
+let inputArg = args[0];
+// let splitArg = inputArg.split("");
+// // let reverseArg = splitArg.reverse();
+// // let joinArg = reverseArg.join("");
+// // console.log(joinArg);
+
+function reverse(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverse(str.substr(1)) + str.charAt(0);
+  }
+}
+
+let reverseStringIs = reverse(inputArg);
+console.log(reverseStringIs);

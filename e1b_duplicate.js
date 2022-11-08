c8607629a837650fa3beee09b6bf34843e00a15d
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-11-08 16:11:00 +0100

    algoEau

--- e1b_duplicate.js
+++ e1b_duplicate.js
@@ -2,9 +2,9 @@ function isDuplicated(x) {
   let xSorted = x.split("").sort();
   for (i = 1; i < xSorted.length; i++) {
     if (xSorted[i - 1] === xSorted[i]) {
-      return true;
+      return "Oui";
     }
   }
-  return false;
+  return "Non";
 }
 console.log(isDuplicated("hey"));

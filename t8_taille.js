643a8c0daaa3e30c99d4b3e154316d43b4777d22
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-11-13 18:19:21 +0100

    algoTerre

--- t8_taille.js
+++ t8_taille.js
@@ -1,7 +1,7 @@
 let args = process.argv.slice(2);
 let inputArg = args[0];
 
-if (isNaN(inputArg)) {
+if (isNaN(inputArg) && args.length === 1) {
   console.log(inputArg.length);
 } else {
   console.log("erreur");

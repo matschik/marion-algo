8dec52527524c76bd0db44e7adad0987673ae075
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-11-14 09:31:27 +0100

    algoEau

--- e3_envers.js
+++ e3_envers.js
@@ -7,3 +7,13 @@ let args = process.argv.slice(2);
 let reverseArgs = args.reverse().join(" ");
 
 console.log(reverseArgs);
+
+// OU
+
+// let args = process.argv.slice(2);
+
+// let arr = [];
+// for (let i = args.length - 1; i >= 0; i--) {
+//   arr.push(args[i]);
+// }
+// console.log(arr.join(" "));

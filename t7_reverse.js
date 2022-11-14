81412e7900bfe094e789cfc1cf14d928e8e0266c
marionsmlg <marion.schimmerling2@hotmail.fr>
2022-11-14 10:01:29 +0100

    algoTerre

--- t7_reverse.js
+++ t7_reverse.js
@@ -4,14 +4,3 @@ let splitArg = inputArg.split("");
 let reverseArg = splitArg.reverse();
 let joinArg = reverseArg.join("");
 console.log(joinArg);
-
-// function reverse(str) {
-//   if (str === "") {
-//     return "";
-//   } else {
-//     return reverse(str.substr(1)) + str.charAt(0);
-//   }
-// }
-
-// let reverseStringIs = reverse(inputArg);
-// console.log(reverseStringIs);
